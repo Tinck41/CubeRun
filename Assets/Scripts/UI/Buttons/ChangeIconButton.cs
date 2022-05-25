c6714699aaccc7856a80da99a285176fd0aa75b1
Valery Glushkov <valery.Glushkov@mytona.com>
2022-05-25 17:36:36 +0900

    saving settings

--- Assets/Scripts/UI/Buttons/ChangeIconButton.cs
+++ Assets/Scripts/UI/Buttons/ChangeIconButton.cs
@@ -1,24 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-enum ButtonState
-{
-    TURN_ON,
-    TURN_OFF
-}
-
 public class ChangeIconButton : MonoBehaviour
 {
     [SerializeField] private Sprite _imageToSwap;
 
     private Image iconHolder;
-    private ButtonState _currentState;
 
     private void Start()
     {
         iconHolder = GetComponent<Image>();
-
-        //_currentState = GetGlobalSettings().isSoundOn();
     }
 
     public void Swap()
---
+++ Assets/Scripts/UI/Windows/SettingsWindow.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class SettingsWindow : MonoBehaviour
+{
+    [SerializeField] private ChangeIconButton _musicIcon;
+    [SerializeField] private ChangeIconButton _soundsIcon;
+
+    private void Start()
+    {
+        var soundsEnabled = PlayerDataHelper.isSoundsEnabled();
+        var musicEnabled = PlayerDataHelper.isMusicEnabled();
+
+        if (!soundsEnabled)
+        {
+            _soundsIcon?.Swap();
+        }
+
+        if (!musicEnabled)
+        {
+            _soundsIcon?.Swap();
+        }
+    }
+
+    public void OnSoundsButtonClick()
+    {
+        PlayerDataHelper.SetSoundsEnabled(!PlayerDataHelper.isSoundsEnabled());
+    }
+
+    public void OnMusicButtonClick()
+    {
+        PlayerDataHelper.SetMusicEnabled(!PlayerDataHelper.isMusicEnabled());
+    }
+
+    public void OnCreaditsButtonClick()
+    {
+
+    }
+}
